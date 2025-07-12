174087df5b861e7abd1da7d6e713766849beda26
Adam Nguyen <160194949+Lunarcell0@users.noreply.github.com>
2025-07-11 23:34:58 -0700

    Cleaned Up Code
    I cleaned up the code of the Hangman Game, with respect to my mentor

--- Hangman.py
+++ Hangman.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import random
 from tkinter import messagebox
+from functools import partial
 #Coordinates of the center of the head
 x = 315
 y = 250
@@ -16,9 +17,8 @@ def space_add(term):
     return temp
 
 
-word = space_add(word_list[random.randint(0, len(word_list) - 1)])
+phrase = space_add(word_list[random.randint(0, len(word_list) - 1)])
 letter_bank = ""
-blank = ""
 window = tk.Tk()
 window.title("Hangman Game")
 window.geometry("650x650")
@@ -26,10 +26,6 @@ window.geometry("650x650")
 #Creates a canvas and hides it until the player clicks on the play button
 canvas = tk.Canvas(window, width=650, height=650)
 canvas.pack_forget()
-
-#Opening Window
-title_label = tk.Label(window, text="Hangman", font="100")
-title_label.place(x=285, y=290)
 
 
 #Function that opens up the Hangman game
@@ -39,57 +35,88 @@ def play():
     title_label.destroy()
 
 
-#Function that takes a user input and determines if it's in the word
-def guess():
+#Function that creates the blank lines based on how many characters there are in the phrase
+def form_blank(word):
+    blank = ""
+    for _ in range(int(len(word) / 2)):
+        blank += "_ "
+    return blank
+
+
+#Function that checks if the letter has already been guessed
+def key_exists(letter):
     global letter_bank
-    global word
-    global index
-    temp2 = ""
-    #Check if the letter has already been guessed
-    if guess_letter_entry.get() in letter_bank:
+    if letter in letter_bank:
         messagebox.showinfo(title='Message', message="You've already guessed this")
         guess_letter_entry.delete(0, tk.END)
         return
     else:
-        letter_bank += guess_letter_entry.get()
-    if guess_letter_entry.get() in word:
-        for letter in word:
-            if letter in letter_bank:
-                temp2 += "{} ".format(letter)
-            elif letter == " ":
-                continue
-            else:
-                temp2 += "_ "
-        canvas.itemconfig(display, text=temp2)
-    elif len(guess_letter_entry.get()) > 1:
-        messagebox.showinfo(title='Message', message="Please type one letter at a time")
-    else:
-        canvas.itemconfigure(man[index], state='hidden')
-        index += 1
-    guess_letter_entry.delete(0, tk.END)
-    #Checks win/lose conditions
-    if index == 6:
+        letter_bank += letter
+
+
+def fill(word):
+    temp = ""
+    for letter in word:
+        if letter in letter_bank:
+            temp += "{} ".format(letter)
+        elif letter == " ":
+            continue
+        else:
+            temp += "_ "
+    return temp
+
+
+#Function that checks win/lose conditions
+def check_conditions(i, temp, word):
+    if i == 6:
         canvas.itemconfigure(game_over_label, state='normal')
         canvas.itemconfigure(display, text=word)
         guess_letter_button.config(state='disabled')
-    elif temp2 == word:
+    elif temp == word:
         canvas.itemconfigure(win_label, state='normal')
         guess_letter_button.config(state='disabled')
+
+
+#Function that removes a body part if the user input was wrong
+def remove_body_part():
+    global index
+    canvas.itemconfigure(man[index], state='hidden')
+    index += 1
+
+
+#Function that takes a user input and determines if it's in the word
+def guess(word):
+    global letter_bank
+    if len(guess_letter_entry.get()) > 1:
+        messagebox.showinfo(title='Message', message="Please type one letter at a time")
+        guess_letter_entry.delete(0, tk.END)
+        return
+    #Check if the letter has already been guessed
+    key_exists(guess_letter_entry.get())
+    if guess_letter_entry.get() in word:
+        canvas.itemconfig(display, text=fill(phrase))
+    else:
+        remove_body_part()
+    guess_letter_entry.delete(0, tk.END)
+    #Checks win/lose conditions
+    check_conditions(index, fill(phrase), phrase)
     window.update()
 
 
+#Opening Window
+title_label = tk.Label(window, text="Hangman", font="100")
+title_label.place(x=285, y=290)
 #Intro UI
 play_button = tk.Button(window, text="Play", font="20", width=7, height=1, command=play)
 play_button.place(x=285, y=325)
-
 
 #Creating the UI
 guess_letter_label = canvas.create_text(x - 100, y + 250, text="Guess a letter:", font='20')
 guess_letter_entry = tk.Entry(window, width=5)
 canvas.create_window(x - 13, y + 251, window=guess_letter_entry)
-guess_letter_button = tk.Button(window, text="Guess", command=guess)
+guess_letter_button = tk.Button(window, text="Guess", command=partial(guess, phrase))
 canvas.create_window(x + 27, y + 251, window=guess_letter_button)
-
+display = canvas.create_text(x, y + 220, text=form_blank(phrase), font='20')
 
 #Creating the Hangman
 base = canvas.create_line(x - 150, y + 210, x + 100, y + 210)
@@ -104,16 +131,10 @@ left_leg = canvas.create_line(x, y + 100, x - 40, y + 200)
 right_leg = canvas.create_line(x, y + 100, x + 40, y + 200)
 man = [right_leg, left_leg, right_arm, left_arm, body, head]
 
-
 #Game Over/Winner UI
 game_over_label = canvas.create_text(x, y - 100, text="Game Over", font='20')
 canvas.itemconfigure(game_over_label, state='hidden')
 win_label = canvas.create_text(x, y - 100, text="You win!!", font='20')
 canvas.itemconfigure(win_label, state='hidden')
 
-
-for _ in range(int(len(word)/2)):
-    blank += "_ "
-display = canvas.create_text(x, y + 220, text=blank, font='20')
 window.mainloop()
-
